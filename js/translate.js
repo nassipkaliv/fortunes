3ed8d4b24c96f32a8878824ed21d67558cbb8f90
nassipkaliv <nassipkaliv@gmail.com>
2024-09-11 15:20:25 +0500

    update

---
+++ js/translate.js
@@ -0,0 +1,32 @@
+const translations = {
+  EN: {
+    title: "Spin the wheel of fortune",
+    buttonText: "SPIN",
+    modalFirstTurn: {
+      congrats: "CONGRATULATIONS",
+      prize: "YOU WIN <br> 150 FREE SPIN",
+      continue: "CONTINUE"
+    },
+    modalSecondTurn: {
+      congrats: "CONGRATULATIONS",
+      prize: "YOU WIN <br> 1500 EUR",
+      continue: "CONTINUE"
+    },
+    languageTitle: "Language"
+  },
+  FR: {
+    title: "Tournez la roue de la fortune",
+    buttonText: "TOURNER",
+    modalFirstTurn: {
+      congrats: "FÉLICITATIONS",
+      prize: "VOUS GAGNEZ <br> 150 TOURS GRATUITS",
+      continue: "CONTINUER"
+    },
+    modalSecondTurn: {
+      congrats: "FÉLICITATIONS",
+      prize: "VOUS GAGNEZ <br> 1500 EUR",
+      continue: "CONTINUER"
+    },
+    languageTitle: "Langue"
+  },
+};
